6e01680595f7131fc171e2548bcc2d3cbe7d0a12
shimizu <dkdkznzn9@gmail.com>
2020-09-14 01:41:00 +0000

    message

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース編集</h2>
+                <h2>ニュースの編集</h2>
                 <form action="{{action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
